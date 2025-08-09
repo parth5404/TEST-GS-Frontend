d98c528b50ff6d3b3c66a69e40fc5ffde6dbdefe
Parth Lahoti <parthlahoti5404@gmail.com>
2025-08-10 00:39:10 +0530

    build resolved

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 //   define: {
 //     'process.env': process.env
 //   }
